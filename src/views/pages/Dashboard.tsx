90f33eddfe6fe13f432f37b5b7f71f3ccfe72de7
Marcin Czaniecki <marcinczaniecki97@gmail.com>
2023-01-12 22:51:39 +0100

    used absolute path in main.tsx and add Global type User

--- src/views/pages/Dashboard.tsx
+++ src/views/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 import { useCustomRouteLoaderData } from '~/hooks/useCustomRouteLoaderData';
 
 const Dashboard = () => {
-  const { user } = useCustomRouteLoaderData<{ user: { name: string } }>('auth');
+  const { user } = useCustomRouteLoaderData<{ user: User }>('auth');
   return (
     <div>
       <h1>Dashboard {user?.name}</h1>
